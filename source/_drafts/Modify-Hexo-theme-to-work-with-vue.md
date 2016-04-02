343b8ad1b0c6888eba5d9d4fc52d2ad0c2ed41c7
gas1121 <jtdzhx@gmail.com>
2016-04-02 20:26:36 +0800

    Use page.js as router and vue-resource to get http data.

--- source/_drafts/Modify-Hexo-theme-to-work-with-vue.md
+++ source/_drafts/Modify-Hexo-theme-to-work-with-vue.md
@@ -15,7 +15,14 @@ categories:
 $('<div/>').append(response.data)
 $('main').replaceWith(receivedHTML.find('main'));
 ```
+由于对前端了解不多，绕了弯路。。。
+挂载位置需要放在页面尾部防止无法找到元素
+弃用了vue-router
+以为需要改造链接来防止跳转，其实不需要
 
 ## 参考资源
 [hexo增加math的显示](http://ranler.github.io/2013/12/31/hexo-math/)
 [jQuery官方文档](http://api.jquery.com/)
+[防止链接直接跳转](https://github.com/vuejs/vue-router/issues/48)
+[page.js](https://github.com/visionmedia/page.js)
+[vue-resource:http](https://github.com/vuejs/vue-resource/blob/master/docs/http.md)
--- themes/next/source/js/custom/main.js
+++ themes/next/source/js/custom/main.js
@@ -1,44 +1,26 @@
 /// <reference path="../../../../../typings/main.d.ts" />
 
 //TODO fix d.ts file include path
-//TODO use vue-router to manage route
 //TODO add variable to judge if use spa mode and include vue files
-//TODO replace main div when click link
+//TODO replace content div when click link
 //TODO make code pretty
 //TODO follow es2015 coding pattern
 //TODO add test and comment
-new Vue({
-  el: '#main',
-  replace: false,
-  data: {
-    message: 'Hello Vue.js!'
-  },
-  
-  ready: function() {
+//TODO solve archives load problem
+function ReplaceHtmlData(context) {
+    console.log(context);
+    console.log($('#content'));
+    Vue.http.get(context.path).then(function (response) {
+        let receivedHTML = $('<div/>').append(response.data);
+        $('#content').fadeOut("slow", function() {
+            $(this).replaceWith(receivedHTML.find('#content'));
+            $('#content').fadeIn("slow");
+        })
+    }, function (response) {
+        // error callback
+    });
+}
 
-      // GET request
-      this.$http.get('/lab').then(function (response) {
-          
-          let receivedHTML = $('<div/>').append(response.data);
-          $('main').replaceWith(receivedHTML.find('main'));
-          //this.message = receivedHTML.find('main')[0].innerHTML;
-
-          // get status
-          response.status;
-
-          // get all headers
-          response.headers();
-
-          // get 'expires' header
-          response.headers('expires');
-
-          // set data on vm
-          this.$set('someData', response.data)
-
-      }, function (response) {
-
-          // error callback
-      });
-
-    }
-})
+//Register and start router
+page('*', ReplaceHtmlData);
+page();
